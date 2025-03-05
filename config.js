36e38a51669c63240b7d059c858beb93b850a7fb
Jinyoung Ahn <ahnjzero@gmail.com>
2025-03-05 14:51:16 +0900

    change output max tokens

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const MODEL = 'claude-3-haiku-20240307';
-export const MAX_TOKENS = 100;
+export const MAX_TOKENS = 1024;
 export const X_API_KEY = process.env.X_API_KEY || '';
 
 export const STORAGE_FILE = 'chat_history.json';
